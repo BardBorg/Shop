fe99085157b5e8d361ed8278c4b36d9dd8bcda34
BardBorg <110245488+BardBorg@users.noreply.github.com>
2023-02-16 16:54:58 +0100

    Update add-product.php

--- add-product.php
+++ add-product.php
@@ -16,81 +16,59 @@
       <h1>Product List</h1>
     </div>
     <hr>
-    <div style="left:40%; position:relative;">
-      <form id="product_form" method="post">
-        <label for="fname">SKU</label>
-        <br>
-        <input type="text" id="sku" name="sku">
-        <br>
-        <label for="fname">Name</label>
-        <br>
-        <input type="text" id="name" name="name">
-        <br>
-        <div id="notificationn"></div>
-        <label for="fname">Price</label>
-        <br>
-        <input type="text" id="price" name="price">
-        <br>
-        <br>
-        <div id="notificationp"></div>
+   <div>
+    <form id="product_form" method="post">
+      <label for="fname">SKU</label><br>
+      <input type="text" id="sku" name="sku"><br>
+      <label for="fname">Name</label><br>
+      <input type="text" id="name" name="name" required><br>
+      <div id="notificationn"></div>
+      <label for="fname">Price</label><br>
+      <input type="text" id="price" name="price"><br><br>
+      <div id="notificationp"></div>
+
+
+    </form>
+    <div id="app">
+      <form v-if="form1" method="post">
+        <label for="fname">Product-DVD</label><br><br>
+        <label for="fname">Please provide DVD size in megabytes</label><br>
+        <input type="text" id="size" name="dvd" @input="dvdinput"><label>MB</label><br><br><br>
+        <div id="notifications"></div>
+
+
       </form>
-      <div id="app">
-        <form v-if="form1" method="post">
-          <label for="fname">Product-DVD</label>
-          <br>
-          <br>
-          <label for="fname">Please provide DVD size in megabytes</label>
-          <br>
-          <input type="number" id="size" name="dvd">
-          <label>MB</label>
-          <br>
-          <br>
-          <br>
-        </form>
-        <form v-if="form2">
-          <label for="fname">Product-Furniture</label>
-          <br>
-          <br>
-          <label for="fname">Please provide dimension in HxWxL format.</label>
-          <br>
-          <br>
-          <label for="Height">Height</label>
-          <br>
-          <input type="number" id="Height" name="furn">
-          <br>
-          <label for="Width">Width</label>
-          <br>
-          <input type="number" id="Width" name="furn">
-          <br>
-          <label for="Length">Length</label>
-          <br>
-          <input type="number" id="Length" name="furn">
-          <br>
-          <br>
-          <br>
-        </form>
-        <form v-if="form3">
-          <label>Product-Book</label>
-          <br>
-          <br>
-          <label>Please provide weight in kilograms</label>
-          <br>
-          <input type="number" id="weight" name="book">
-          <label>KG</label>
-          <br>
-          <br>
-          <br>
-        </form>
+      <form v-if="form2">
+        <label for="fname">Product-Furniture</label><br><br>
+        <label for="fname">Please provide dimension in HxWxL format.</label><br><br>
+        <label for="Height">Height</label><br>
+        <input type="text" id="height" name="furn" @input="hinput"><br>
+        <div id="notificationh"></div>
+        <label for="Width">Width</label><br>
+        <input type="text" id="width" name="furn" @input="winput"><br>
+        <div id="notificationw"></div>
+        <label for="Length">Length</label><br>
+        <input type="text" id="length" name="furn" @input="linput"><br><br>
+        <div id="notificationl"></div></br>
+
+      </form>
+      <form v-if="form3">
+        <label>Product-Book</label><br><br>
+        <label>Please provide weight in kilograms</label><br>
+        <input type="text" id="weight" name="book" @input="bookinput"><label>KG</label><br><br><br>
+        <div id="notificationb"></div>
+
+
+      </form>
         <label style="font-weight:bold;">Select another product</label>
         <br>
         <select v-model="selectedOption">
           <option id="productType" v-for="option in options" :key="option.value">{{ option.text }}</option>
         </select>
       </div>
-    </div> <?php
-
-
-  ?> <script>
+    </div> 
+  
+  <script>
       //Vue component for selecting product type
       var app = new Vue({
         el: '#app',
@@ -113,25 +91,98 @@
           }
         },
         watch: {
-          // Get the selected option value
-          selectedOption: function(newValue, oldValue) {
-            // Hiding/showing product forms
-            if (newValue === 'DVD') {
-              this.form1 = true;
-              this.form3 = false;
-              this.form2 = false;
-            } else if (newValue === 'Furniture') {
-              this.form2 = true;
-              this.form1 = false;
-              this.form3 = false;
-            } else if (newValue === 'Book') {
-              this.form3 = true;
-              this.form2 = false;
-              this.form1 = false;
-            }
-          }
+      selectedOption: function(newValue, oldValue) {
+
+        if (newValue === 'DVD') {
+
+          this.form1 = true;
+          this.form2 = false;
+          this.form3 = false;
+        } else if (newValue === 'Furniture') {
+
+          this.form2 = true;
+          this.form1 = false;
+          this.form3 = false;
+        } else if (newValue === 'Book') {
+
+          this.form3 = true;
+          this.form2 = false;
+          this.form1 = false;
         }
-      });
+
+      }
+    },
+    methods: {
+
+      dvdinput(event) {
+        var dvd2 = document.getElementById("size");
+        var notsize = document.getElementById("notifications");
+
+
+
+        var contain2 = /^[a-zA-Z]+$/.test(dvd2.value);
+        if (contain2) {
+          notsize.textContent = "Please provide the data of indicated type";
+        } else {
+          notsize.textContent = "";
+        }
+      },
+      bookinput(event) {
+        var book2 = document.getElementById("weight");
+        var notbook = document.getElementById("notificationb");
+
+
+
+        var contain3 = /^[a-zA-Z]+$/.test(book2.value);
+        if (contain3) {
+          notbook.textContent = "Please provide the data of indicated type";
+        } else {
+          notbook.textContent = "";
+        }
+      },
+      hinput(event) {
+        var notheight = document.getElementById("notificationh");
+        var height2 = document.getElementById("height");
+
+
+
+
+        var contain4 = /^[a-zA-Z]+$/.test(height2.value);
+        if (contain4) {
+          notheight.textContent = "Please provide the data of indicated type";
+        } else {
+          notheight.textContent = "";
+        }
+      },
+      winput(event) {
+        var width2 = document.getElementById("width");
+        var notwidth = document.getElementById("notificationw");
+
+
+        var contain5 = /^[a-zA-Z]+$/.test(width2.value);
+        if (contain5) {
+          notwidth.textContent = "Please provide the data of indicated type";
+        } else {
+          notwidth.textContent = "";
+        }
+      },
+      linput(event) {
+        var length2 = document.getElementById("length");
+        var notlength = document.getElementById("notificationl");
+
+
+
+        var contain6 = /^[a-zA-Z]+$/.test(length2.value);
+        if (contain6) {
+          notlength.textContent = "Please provide the data of indicated type";
+        } else {
+          notlength.textContent = "";
+        }
+      }
+    }
+
+
+  });
       document.getElementById("new").addEventListener("click", send);
       //Function for passing values to insert.php where is the DB logic.
       function send() {
